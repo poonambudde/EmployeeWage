4d340d8423cee674ac9a4673a1539222ab3106b6
poonambudde <buddepoonam123@gmail.com>
2022-02-14 17:02:53 +0530

    [Poonam] ADDED: Calculate Daily Employee Wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,11 +6,24 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
+            int PRESENT = 0;
+            int PARTIAL_PRESENT = 1;
+            int WAGE_PER_HOUR = 8;
+            int FULL_TIME_WORKING_HOUR = 20;
+            int wage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == 0)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == PRESENT)
             {
                 Console.WriteLine("Employee is present");
+                wage = FULL_TIME_WORKING_HOUR * WAGE_PER_HOUR;
+                Console.WriteLine($"WAGE:{wage}");
+            }
+            else if (empCheck == PARTIAL_PRESENT)
+            {
+                Console.WriteLine("Employee is partial present");
+                Console.WriteLine($"WAGE:{wage}");
+
             }
             else
             {
