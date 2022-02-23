b6e7ede8f73c3e50bf500b62b50b071b38b7879c
poonambudde <buddepoonam123@gmail.com>
2022-02-23 19:33:55 +0530

    [Poonam] ADDED: Store the Daily Wage along with the Total Wage

--- EmployeeWage/EmpWageBuilderArray.cs
+++ EmployeeWage/EmpWageBuilderArray.cs
@@ -9,18 +9,20 @@ namespace Employee_Wage
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        private int numOfCompany = 0;
-        List<CompanyEmpWage> companyEmpWageList;
 
+        List<CompanyEmpWage> companyEmpWageList;
+        Dictionary<string, CompanyEmpWage> companyToEmpWageMap;
         public EmpWageBuilderArray()
         {
             this.companyEmpWageList = new List<CompanyEmpWage>();
+            this.companyToEmpWageMap = new Dictionary<string, CompanyEmpWage>();
         }
 
         public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
             CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
             this.companyEmpWageList.Add(companyEmpWage);
+            this.companyToEmpWageMap.Add(company, companyEmpWage);
         }
 
         public void computeEmpWage()
