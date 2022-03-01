76c4fce25e791dcd36e04ea5bc02de5f2e48818f
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-01 17:08:12 +0530

    [Poonam] ADDED: Compute Employee Wage for multiple companies

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,6 +6,7 @@ namespace EmployeeWage
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
+        
         public static int computeEmpWage(string company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
         { 
             int empHrs = 0;
